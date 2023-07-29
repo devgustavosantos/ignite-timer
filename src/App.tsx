06026672b074effd59ab11e27ecbd3be238acd14
Gustavo Santos <devgustavosantos@gmail.com>
2023-07-29 13:54:07 -0300

    feat: creates the general styles of the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { ThemeProvider } from 'styled-components';
+
 import { Header } from './styles';
+import { COLORS } from './styles/colors';
+import { Styles } from './styles/index';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <p>Teste</p>
-    </div>
+    <ThemeProvider theme={COLORS}>
+      <Styles />
+      <div>
+        <Header />
+        <p>Teste</p>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/global.ts
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const Global = createGlobalStyle`
+  html {
+    font-size: 6.25%;
+  }
+
+  body,
+  input,
+  button,
+  textarea {
+    color: ${({ theme }) => theme.WHITE_900};
+    outline: none;
+    font-size: 16rem;
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body {
+    width: 100%;
+    min-height: 100vh;
+    background-color: ${({ theme }) => theme.BLACK_900};
+  }
+
+  button {
+    border: none;
+    background-color: transparent;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button,
+  a {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+
+  button:hover,
+  a:hover {
+    filter: brightness(0.9);
+  }
+
+  a:visited {
+    color: inherit;
+  }
+`;
