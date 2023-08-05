d97c9e0696491a5f800ca2ca0aea4a1d389880f5
Gustavo Santos <devgustavosantos@gmail.com>
2023-08-04 23:19:12 -0300

    feat: start the styles of the form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import { Play } from '@phosphor-icons/react';
+
 import { Header } from '../../components/Header';
 import * as S from './styles';
 
@@ -6,6 +8,42 @@ export function Home() {
     <S.Container>
       <S.Main>
         <Header />
+
+        <S.Form>
+          <S.Label>
+            Vou trabalhar em
+            <S.TaskInput
+              type="text"
+              placeholder="Dê um nome para o seu projeto"
+            />
+            durante
+            <S.MinutesContainer>
+              <S.MinutesButtons type="button">
+                <S.MinusIcon />
+              </S.MinutesButtons>
+              <S.MinutesInput
+                type="number"
+                placeholder="00"
+              />
+              <S.MinutesButtons type="button">
+                <S.PlusIcon />
+              </S.MinutesButtons>
+            </S.MinutesContainer>
+            minutos.
+          </S.Label>
+
+          <div>
+            <span>0</span>
+            <span>0</span>
+            <span>:</span>
+            <span>0</span>
+            <span>0</span>
+          </div>
+
+          <button>
+            <Play /> Começar
+          </button>
+        </S.Form>
       </S.Main>
     </S.Container>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,19 +1,115 @@
-import { styled } from 'styled-components';
+import { Minus, Plus } from '@phosphor-icons/react';
+import { css, styled } from 'styled-components';
+
+import { BREAKPOINTS } from '../../styles/breakpoints';
 
 const Container = styled.div`
-  padding: 10rem;
-  display: flex;
-  justify-content: center;
   align-items: center;
+  display: flex;
   flex-grow: 1;
+  justify-content: center;
+  padding: 10rem;
 `;
 
 const Main = styled.main`
-  max-width: 1120rem;
-  width: 100%;
-  padding: 40rem;
   background-color: ${({ theme }) => theme.BLACK_800};
   border-radius: 8rem;
+  max-width: 1120rem;
+  padding: 40rem;
+  width: 100%;
 `;
 
-export { Container, Main };
+const Form = styled.form`
+  font-size: 18rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 28.8rem;
+  margin: 72rem auto 120rem;
+  max-width: 656rem;
+`;
+
+const Label = styled.label`
+  align-items: center;
+  display: flex;
+  gap: 5rem;
+  justify-content: space-between;
+  width: 100%;
+
+  @media only screen and (max-width: ${BREAKPOINTS.medium}) {
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+`;
+
+const TaskInput = styled.input`
+  background-color: transparent;
+  border: 0;
+  border-bottom: 2px solid ${({ theme }) => theme.GRAY_900};
+  color: ${({ theme }) => theme.GRAY_900};
+  font-size: inherit;
+  font-weight: inherit;
+  min-width: 250rem;
+  text-align: center;
+`;
+
+const MinutesContainer = styled.div`
+  align-items: center;
+  border-bottom: 2px solid ${({ theme }) => theme.GRAY_900};
+  color: ${({ theme }) => theme.GRAY_900};
+  display: flex;
+  justify-content: space-between;
+  min-width: 80px;
+`;
+
+const MinutesInput = styled.input`
+  background-color: transparent;
+  border: 0;
+  font-size: inherit;
+  font-weight: inherit;
+  text-align: center;
+  width: 21rem;
+
+  /* stylelint-disable-next-line */
+  -moz-appearance: textfield;
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    margin: 0;
+
+    /* stylelint-disable-next-line */
+    -webkit-appearance: none;
+  }
+`;
+
+const MinutesButtons = styled.button`
+  color: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+  display: flex;
+  align-items: center;
+`;
+
+const iconsStyles = css`
+  font-size: 16rem;
+`;
+
+const MinusIcon = styled(Minus)`
+  ${iconsStyles}
+`;
+
+const PlusIcon = styled(Plus)`
+  ${iconsStyles}
+`;
+
+export {
+  Container,
+  Form,
+  Label,
+  Main,
+  MinusIcon,
+  MinutesButtons,
+  MinutesContainer,
+  MinutesInput,
+  PlusIcon,
+  TaskInput,
+};
