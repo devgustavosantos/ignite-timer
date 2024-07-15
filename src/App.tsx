4800d5b34667679149feabab237e1a7c78057fc8
Gustavo Santos <devgustavosantos@gmail.com>
2024-07-15 07:29:37 -0300

    refactor: isolates general style logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 
 import { Router } from './Routes';
-import { COLORS } from './styles/colors';
 import { Styles } from './styles/index';
 
 function App() {
   return (
-    <ThemeProvider theme={COLORS}>
-      <Styles />
-
-      <BrowserRouter>
+    <BrowserRouter>
+      <Styles>
         <Router />
-      </BrowserRouter>
-    </ThemeProvider>
+      </Styles>
+    </BrowserRouter>
   );
 }
 
