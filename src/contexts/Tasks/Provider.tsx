ef18fdf4ea3226d674d1ede8e63c9c0d6cbab248
Gustavo Santos <devgustavosantos@gmail.com>
2024-08-10 12:22:24 -0300

    feat: saves tasks in memory

--- src/contexts/Tasks/Provider.tsx
+++ src/contexts/Tasks/Provider.tsx
@@ -1,14 +1,22 @@
 import { produce } from 'immer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import superjson from 'superjson';
 
 import { CreateTaskType, TaskType } from '@/types/task';
 
 import { TasksContext } from './';
 import { TasksProviderProps } from './types';
+import { LOCAL_STORAGE_KEY } from './utils';
 
 export function TasksProvider({ children }: TasksProviderProps) {
   const [currentTask, setCurrentTask] = useState<TaskType | null>(null);
-  const [tasks, setTasks] = useState<TaskType[]>([]);
+  const [tasks, setTasks] = useState<TaskType[]>(() => {
+    const savedTasks = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (!savedTasks) return [];
+
+    const savedTasksParsed = superjson.parse<TaskType[]>(savedTasks);
+    return savedTasksParsed;
+  });
 
   function createTask({ name, desiredTime }: CreateTaskType) {
     if (currentTask) return;
@@ -54,6 +62,12 @@ export function TasksProvider({ children }: TasksProviderProps) {
     setCurrentTask(null);
     setTasks(nextTasks);
   }
+
+  useEffect(() => {
+    if (!tasks.length) return;
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, superjson.stringify(tasks));
+  }, [tasks]);
 
   return (
     <TasksContext.Provider
---
+++ src/contexts/Tasks/utils.ts
@@ -0,0 +1,3 @@
+import { name as appName, version as appVersion } from '@/../package.json';
+
+export const LOCAL_STORAGE_KEY = `${appName}.task@${appVersion}`;
